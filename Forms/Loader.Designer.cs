27b7a4d369cdc3e644e3be3e9d8f3004935d2d1f
PKTINOS <parisoplop@gmail.com>
2021-01-25 20:52:07 +0200

    Loader patches

--- Forms/Loader.Designer.cs
+++ Forms/Loader.Designer.cs
@@ -32,13 +32,31 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.usernameInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.loginBox = new System.Windows.Forms.GroupBox();
+            this.loginManual = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.codeInput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radiopanel = new System.Windows.Forms.Panel();
+            this.loginBox.SuspendLayout();
+            this.loginManual.SuspendLayout();
+            this.radiopanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // progressBar1
             // 
             this.progressBar1.Location = new System.Drawing.Point(12, 297);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(359, 23);
+            this.progressBar1.Size = new System.Drawing.Size(371, 23);
             this.progressBar1.TabIndex = 0;
             // 
             // richTextBox1
@@ -51,22 +69,182 @@
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
-            // backgroundWorker1
+            // usernameInput
             // 
-            //this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.usernameInput.Location = new System.Drawing.Point(13, 52);
+            this.usernameInput.Name = "usernameInput";
+            this.usernameInput.Size = new System.Drawing.Size(153, 20);
+            this.usernameInput.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username for iee.ihu.gr:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.Location = new System.Drawing.Point(13, 103);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.Size = new System.Drawing.Size(153, 20);
+            this.passwordInput.TabIndex = 6;
+            this.passwordInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordInput_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // loginBox
+            // 
+            this.loginBox.Controls.Add(this.label1);
+            this.loginBox.Controls.Add(this.button2);
+            this.loginBox.Controls.Add(this.usernameInput);
+            this.loginBox.Controls.Add(this.passwordInput);
+            this.loginBox.Controls.Add(this.label2);
+            this.loginBox.Enabled = false;
+            this.loginBox.Location = new System.Drawing.Point(389, 12);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(181, 172);
+            this.loginBox.TabIndex = 8;
+            this.loginBox.TabStop = false;
+            this.loginBox.Text = "Login form";
+            // 
+            // loginManual
+            // 
+            this.loginManual.Controls.Add(this.button1);
+            this.loginManual.Controls.Add(this.codeInput);
+            this.loginManual.Controls.Add(this.label4);
+            this.loginManual.Controls.Add(this.button3);
+            this.loginManual.Enabled = false;
+            this.loginManual.Location = new System.Drawing.Point(389, 12);
+            this.loginManual.Name = "loginManual";
+            this.loginManual.Size = new System.Drawing.Size(183, 171);
+            this.loginManual.TabIndex = 14;
+            this.loginManual.TabStop = false;
+            this.loginManual.Text = "Manual login";
+            this.loginManual.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Open authorization page";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // codeInput
+            // 
+            this.codeInput.Location = new System.Drawing.Point(41, 72);
+            this.codeInput.Name = "codeInput";
+            this.codeInput.Size = new System.Drawing.Size(101, 20);
+            this.codeInput.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Code:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 20);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Submit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 78);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Your credenitials are not stored and \r\nare only used to connect to the api.\r\nHowe" +
+    "ver, you have the choice of\r\nconnecting yourself by switching \r\nto manual login." +
+    "\r\n\r\n";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(2, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 17);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Silent login";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(84, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.Text = "Manual login";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radiopanel
+            // 
+            this.radiopanel.Controls.Add(this.radioButton1);
+            this.radiopanel.Controls.Add(this.radioButton2);
+            this.radiopanel.Enabled = false;
+            this.radiopanel.Location = new System.Drawing.Point(387, 256);
+            this.radiopanel.Name = "radiopanel";
+            this.radiopanel.Size = new System.Drawing.Size(183, 22);
+            this.radiopanel.TabIndex = 17;
             // 
             // Loader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(383, 332);
+            this.ClientSize = new System.Drawing.Size(579, 332);
+            this.Controls.Add(this.radiopanel);
+            this.Controls.Add(this.loginManual);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.loginBox);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.progressBar1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Loader";
             this.Text = "Loader";
             this.Load += new System.EventHandler(this.Loader_Load);
+            this.loginBox.ResumeLayout(false);
+            this.loginBox.PerformLayout();
+            this.loginManual.ResumeLayout(false);
+            this.loginManual.PerformLayout();
+            this.radiopanel.ResumeLayout(false);
+            this.radiopanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +253,20 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox usernameInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox loginBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox codeInput;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox loginManual;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel radiopanel;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Forms/Loader.cs
+++ Forms/Loader.cs
@@ -8,7 +8,10 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -29,12 +32,49 @@ namespace AnnouncementHelper.Forms
             BackColor = Program.BgColor;
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
-            DoWork();
+            BeforeAccessTokenWork();
         }
 
-        private async void DoWork()
-        {
 
+        private async void BeforeAccessTokenWork()
+        {
+            await Initialize();
+
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AnnouncementHelper\\refresh.token"))
+            {
+                StringEdit.Out("Refresh token found!", ref richTextBox1,StringEdit.OutType.Success);
+                await GetAccessTokenUsingRefresh(File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AnnouncementHelper\\refresh.token"));
+                AfterAccessTokenWork();
+            }
+            else
+            {
+                StringEdit.Out("Please choose your login method and login.", ref richTextBox1);
+                radiopanel.Enabled = true;
+                loginBox.Enabled = true;
+                loginManual.Enabled = true;
+            }
+
+        }
+
+        private async void AfterAccessTokenWork()
+        {
+            if (Program.access_token == "error")
+            {
+                StringEdit.Out("Error when loading access token. Please report to the developer.", ref richTextBox1, StringEdit.OutType.Error);
+                return;
+            }
+            var task1 = LoadCategories();
+            var task2 = LoadUserProfile();
+            var task3 = LoadAnnouncements();
+            await Task.WhenAll(task1, task2, task3);
+
+            this.Hide();
+            var organizer = new Organizer();
+            organizer.Closed += (s, args) => this.Close();
+            organizer.Show();
+        }
+        private async Task Initialize()
+        {
             RegistryKey rk = Registry.CurrentUser.OpenSubKey
     ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
             if (rk.GetValue("AnnouncementHelper") != null)
@@ -74,38 +114,6 @@ namespace AnnouncementHelper.Forms
                 UseProxy = false,
             };
             client = new HttpClient(hch);
-
-            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AnnouncementHelper\\refresh.token"))
-            {
-                StringEdit.Out("Βρέθηκε refresh token...", ref richTextBox1);
-                await GetAccessTokenUsingRefresh(File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AnnouncementHelper\\refresh.token"));
-            }
-            else
-            {
-                StringEdit.Out("Παρακαλώ δώστε εξουσιοδότηση στην εφαρμογή.", ref richTextBox1);
-                StringEdit.Out("Έπειτα, εισάγετε τον αριθμό \"code\" που βλέπετε:", ref richTextBox1);
-                Process.Start("https://login.iee.ihu.gr/authorization/?client_id=" + Program.CLIENT_ID + "&response_type=code&scope=announcements,notifications,profile&redirect_uri=https://users.it.teithe.gr/~it185246/accepted.html");
-                Focus();
-                richTextBox1.Refresh();
-                string code = Interaction.InputBox("Code:", "Insert code", "", 0, 0);
-                Program.access_token = await GetAccessToken(code);
-            }
-
-            if (Program.access_token == "error")
-            {
-                StringEdit.Out("Το πρόγραμμα τωρα θα τερματιστεί.", ref richTextBox1, StringEdit.OutType.Error);
-                await Task.Delay(2000);
-                Close();
-            }
-            var task1 = LoadCategories();
-            var task2 = LoadUserProfile();
-            var task3 = LoadAnnouncements();
-            await Task.WhenAll(task1,task2,task3);
-
-            this.Hide();
-            var organizer = new Organizer();
-            organizer.Closed += (s, args) => this.Close();
-            organizer.Show();
         }
 
         private async Task LoadCategories(){
@@ -124,8 +132,7 @@ namespace AnnouncementHelper.Forms
                     if (s.Contains("error"))
                     {
                         StringEdit.Out(s, ref richTextBox1);
-                        await Task.Delay(2000);
-                        Close();
+                        return;
                     }
                     Category temp;
                     temp = JsonConvert.DeserializeObject<Category>(s);
@@ -135,14 +142,12 @@ namespace AnnouncementHelper.Forms
                 }
                 if (Program.Categories.Count > 0)
                 {
-                    StringEdit.Out("Περάστηκαν " + Program.Categories.Count + " κατηγορίες ανακοινώσεων.", ref richTextBox1, StringEdit.OutType.Success);
+                    StringEdit.Out("Found " + Program.Categories.Count + " categories.", ref richTextBox1, StringEdit.OutType.Success);
                 }
                 else
                 {
-                    StringEdit.Out("Κάτι πήγε στραβά κατά την λήψη κατηγοριών ανακοινώσεων.", ref richTextBox1, StringEdit.OutType.Error);
-                    StringEdit.Out("Το πρόγραμμα τωρα θα τερματιστεί.", ref richTextBox1, StringEdit.OutType.Error);
-                    await Task.Delay(2000);
-                    Close();
+                    StringEdit.Out("Something went wrong while fetching the categories.", ref richTextBox1, StringEdit.OutType.Error);
+                    return;
                 }
             }
         }
@@ -158,17 +163,15 @@ namespace AnnouncementHelper.Forms
                 Program.UserProfile = JsonConvert.DeserializeObject<Profile>(responseString);
                 if (Program.UserProfile.GivenName == string.Empty)
                 {
-                    StringEdit.Out("Κάτι πήγε στραβά κατά την λήψη profile.", ref richTextBox1, StringEdit.OutType.Error);
-                    StringEdit.Out("Το πρόγραμμα τωρα θα τερματιστεί.", ref richTextBox1, StringEdit.OutType.Error);
-                    await Task.Delay(2000);
-                    Close();
+                    StringEdit.Out("Something went wrong while fetching the profile.", ref richTextBox1, StringEdit.OutType.Error);
+                    return;
                 }
             }
-            StringEdit.Out("Βρέθηκε εξάμηνο:" + Program.UserProfile.Sem, ref richTextBox1);
+            StringEdit.Out("Semester found:" + Program.UserProfile.Sem, ref richTextBox1);
         }
 
         private async Task LoadAnnouncements(){
-            StringEdit.Out("Κατέβασμα ανακοινώσεων...", ref richTextBox1);
+            StringEdit.Out("Fetching announcements...", ref richTextBox1);
             using (var request = new HttpRequestMessage(new HttpMethod("GET"), "http://api.iee.ihu.gr/announcements"))
             {
                 request.Headers.TryAddWithoutValidation("x-access-token", Program.access_token);
@@ -187,14 +190,12 @@ namespace AnnouncementHelper.Forms
                 }
                 if (Program.Announcements.Count > 0)
                 {
-                    StringEdit.Out("Κατέβηκαν " + Program.Announcements.Count + " ανακοινώσεις.", ref richTextBox1, StringEdit.OutType.Success);
+                    StringEdit.Out("Found " + Program.Announcements.Count + " announcements.", ref richTextBox1, StringEdit.OutType.Success);
                 }
                 else
                 {
-                    StringEdit.Out("Κάτι πήγε στραβά κατά την λήψη ανακοινώσεων.", ref richTextBox1, StringEdit.OutType.Error);
-                    StringEdit.Out("Το πρόγραμμα τωρα θα τερματιστεί.", ref richTextBox1, StringEdit.OutType.Error);
-                    await Task.Delay(2000);
-                    Close();
+                    StringEdit.Out("Something went wrong while fetching the announcements.", ref richTextBox1, StringEdit.OutType.Error);
+                    return;
                 }
             }
         }
@@ -218,28 +219,24 @@ namespace AnnouncementHelper.Forms
             MatchCollection matches = Regex.Matches(responseString, "\"([^\"]*)\"");
             if (matches.Count == 0)
             {
-                StringEdit.Out("Δεν βρέθηκε JSON απάντηση κατά την προσπάθεια απόκτησης Access Token.", ref richTextBox1, StringEdit.OutType.Error);
+                StringEdit.Out("JSON answer missing error.", ref richTextBox1, StringEdit.OutType.Error);
             }
             else
             {
                 if (matches[0].ToString().Contains("access_token"))
                 {
-                    StringEdit.Out("Access Token λήφθηκε μέσω refresh token!", ref richTextBox1, StringEdit.OutType.Success);
+                    StringEdit.Out("Got access token using the refresh token!", ref richTextBox1, StringEdit.OutType.Success);
                     File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AnnouncementHelper\\refresh.token", matches[5].ToString().Replace("\"", ""));
                     Program.access_token = matches[1].ToString().Replace("\"", "");
                     return;
                 }
                 else
                 {
-                    StringEdit.Out("Error σε μορφή JSON:" + Environment.NewLine + responseString, ref richTextBox1, StringEdit.OutType.Error);
-                    StringEdit.Out("Λογικα έληξε το refresh_token", ref richTextBox1);
-                    StringEdit.Out("Παρακαλώ ξαναδώστε εξουσιοδότηση", ref richTextBox1);
+                    StringEdit.Out("JSON error:" + Environment.NewLine + responseString, ref richTextBox1, StringEdit.OutType.Error);
+                    StringEdit.Out("Possibly expired refresh_token.", ref richTextBox1);
+                    StringEdit.Out("Please restart the program.", ref richTextBox1);
                     if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AnnouncementHelper\\refresh.token"))
                         File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AnnouncementHelper\\refresh.token");
-                    Process.Start("https://login.iee.ihu.gr/authorization/?client_id=" + Program.CLIENT_ID + "&response_type=code&scope=announcements,notifications,profile&redirect_uri=https://users.iee.ihu.gr/~it185246/accepted.html");
-                    Focus();
-                    richTextBox1.Refresh();
-                    Program.access_token = await GetAccessToken(Interaction.InputBox("Code:", "Insert code", "", 0, 0));
                     return;
                 }
             }
@@ -268,24 +265,100 @@ namespace AnnouncementHelper.Forms
             if (matches.Count == 0)
             {
 
-                StringEdit.Out("Δεν βρέθηκε JSON απάντηση κατά την προσπάθεια απόκτησης Access Token.", ref richTextBox1, StringEdit.OutType.Error);
+                StringEdit.Out("JSON answer missing error.", ref richTextBox1, StringEdit.OutType.Error);
                 return "error";
             }
             else
             {
                 if (matches[0].ToString().Contains("access_token"))
                 {
-                    StringEdit.Out("Access Token λήφθηκε!", ref richTextBox1, StringEdit.OutType.Success);
+                    StringEdit.Out("Access token fetched!", ref richTextBox1, StringEdit.OutType.Success);
                     File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AnnouncementHelper\\refresh.token", matches[5].ToString().Replace("\"", ""));
                     return matches[1].ToString().Replace("\"", "");
                 }
                 else
                 {
-                    StringEdit.Out("Error σε μορφή JSON:" + Environment.NewLine + responseString, ref richTextBox1);
+                    StringEdit.Out("JSON error:" + Environment.NewLine + responseString, ref richTextBox1);
                     return "error";
                 }
             }
 
+        }
+
+        private async Task<string> GetCodeWithLoginForm(string username, string password)
+        {
+            string link = "https://login.iee.ihu.gr/login?client_id=" + Program.CLIENT_ID + "&response_type=code&scope=announcements,notifications,profile&redirect_uri=https://users.it.teithe.gr/~it185246/accepted.html";
+            HttpClientHandler hch = new HttpClientHandler
+            {
+                Proxy = null,
+                UseProxy = false,
+            };
+            client = new HttpClient(hch);
+            var values = new Dictionary<string, string>
+            {
+                { "username", username },
+                { "password", password },
+                { "saveMeCheck" , "true"}
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync(link, content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            using (var request = new HttpRequestMessage(new HttpMethod("GET"), link))
+            {
+                ServicePointManager.ServerCertificateValidationCallback = delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+                var xresponse = await client.SendAsync(request);
+                string xresponseString = await xresponse.Content.ReadAsStringAsync();
+
+                return xresponse.RequestMessage.RequestUri.ToString().Split('=')[1].Split('&')[0];
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                loginBox.Visible = true;
+                loginManual.Visible = false;
+            }
+            else
+            {
+                loginBox.Visible = false;
+                loginManual.Visible = true;
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            loginBox.Enabled = false;
+            string code = await GetCodeWithLoginForm(usernameInput.Text, passwordInput.Text);
+            Program.access_token = await GetAccessToken(code);
+            AfterAccessTokenWork();
+        }
+
+        private void passwordInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button2_Click(this, new EventArgs());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string link = "https://login.iee.ihu.gr/login?client_id=" + Program.CLIENT_ID + "&response_type=code&scope=announcements,notifications,profile&redirect_uri=https://users.it.teithe.gr/~it185246/accepted.html";
+            Process.Start(link);
+            StringEdit.Out("Please use the opened browser window to authorize and paste the code back here.", ref richTextBox1, StringEdit.OutType.Alert);
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            string code = codeInput.Text;
+            Program.access_token = await GetAccessToken(code);
+            AfterAccessTokenWork();
         }
     }
 }
